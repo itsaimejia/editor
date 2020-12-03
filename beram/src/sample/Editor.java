88d887e31e0e796313c0122fb174fffe6f1c61ac
Gloria-Berenice-Hernandez <berebonita_29@hotmail.com>
2020-12-02 20:38:04 -0800

    Primera Version
    fkanfq2jipwñkfv

---
+++ beram/src/sample/Editor.java
@@ -0,0 +1,15 @@
+package sample;
+
+import javax.swing.*;
+
+public class Editor {
+
+    private JMenuBar menuBar1;
+    private JMenu menu1;
+    private JPanel panel1;
+    private JMenu menu2;
+
+    private void createUIComponents() {
+
+    }
+}
