891670d9c0ffa2f2585df1ce2155380cf7924a06
Gloria-Berenice-Hernandez <berebonita_29@hotmail.com>
2020-12-06 22:32:35 -0800

    avance 2.1

--- beram/src/sample/Controller.java
+++ beram/src/sample/Controller.java
@@ -1,20 +1,57 @@
 package sample;
 
 import javafx.fxml.FXML;
-
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import javafx.scene.control.TextArea;
 public class Controller {
+
+    private boolean flag=true;
+    private String aux_lector;
+    @FXML
+    public static TextArea text_area;
     @FXML
     private void onOpen()
     {
+        FileChooser fc= new FileChooser();
+        FileChooser.ExtensionFilter txt = new FileChooser.ExtensionFilter("Archivo de texto (.txt)" ,".txt");
+        FileChooser.ExtensionFilter pdf = new FileChooser.ExtensionFilter("PDF file(.pdf)" ,".pdf");
+        FileChooser.ExtensionFilter dat = new FileChooser.ExtensionFilter("Binary Code (.dat)" ,".dat");
+        fc.getExtensionFilters().add(txt);
+        fc.getExtensionFilters().add(pdf);
+        fc.getExtensionFilters().add(dat);
+        fc.showOpenDialog(new Stage());
+
+        //Capturar archivo a poner en JTextArea
+        File  archivo= new File (fc.getInitialFileName());
+        text_area.setText("");
+        try {
+            if (archivo != null){
+                FileReader archivos= new FileReader(archivo);
+                BufferedReader leer = new BufferedReader(archivos);
+                while ((aux_lector = leer.readLine()) != null) {
+                    text_area.appendText(aux_lector+"\n");
+                }
+                leer.close();
+            }
+        }catch (IOException e) {
+            System.out.println(e.toString());
+        }
 
     }
+
     @FXML
     private void onSave()
     {
 
     }
+
     @FXML
-    private void onSaveAs()
+    private void onSaveas()
     {
 
     }
@@ -24,15 +61,9 @@ public class Controller {
 
     }
     @FXML
-    private void onExit()
+    private void onClose()
     {
-      System.exit(0);
+        System.exit(0);
     }
-    @FXML
-    private void onPrint()
-    {
-
-    }
-
 
 }
