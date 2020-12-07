3faebd75de7ba55a09c623bcb24b31c36136a9c5
Gloria-Berenice-Hernandez <berebonita_29@hotmail.com>
2020-12-06 20:35:55 -0800

    Menus
    Interfaz semi-terminada

--- beram/src/sample/Controller.java
+++ beram/src/sample/Controller.java
@@ -1,4 +1,38 @@
 package sample;
 
+import javafx.fxml.FXML;
+
 public class Controller {
+    @FXML
+    private void onOpen()
+    {
+
+    }
+    @FXML
+    private void onSave()
+    {
+
+    }
+    @FXML
+    private void onSaveAs()
+    {
+
+    }
+    @FXML
+    private void onNew()
+    {
+
+    }
+    @FXML
+    private void onExit()
+    {
+      System.exit(0);
+    }
+    @FXML
+    private void onPrint()
+    {
+
+    }
+
+
 }
