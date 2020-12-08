33e51cc8dcfbcdc7c6dc00f6cc554d3cd1f07d55
itsaimejia <33639089+itsaimejia@users.noreply.github.com>
2020-12-07 19:04:44 -0800

    filechooser

--- beram/src/sample/Controller.java
+++ beram/src/sample/Controller.java
@@ -1,48 +1,46 @@
 package sample;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
 import javafx.stage.FileChooser;
+import javafx.stage.FileChooser.ExtensionFilter;
 import javafx.stage.Stage;
+
+import java.awt.event.ActionEvent;
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import javafx.scene.control.TextArea;
+
 public class Controller {
 
-    private boolean flag=true;
-    private String aux_lector;
     @FXML
-    public static TextArea text_area;
+    static TextArea text_area;
+
     @FXML
-    private void onOpen()
+    private void onOpen() throws IOException
     {
-        FileChooser fc= new FileChooser();
-        FileChooser.ExtensionFilter txt = new FileChooser.ExtensionFilter("TXT file (.txt)" ,".txt");
-        FileChooser.ExtensionFilter pdf = new FileChooser.ExtensionFilter("PDF file(.pdf)" ,".pdf");
-        FileChooser.ExtensionFilter dat = new FileChooser.ExtensionFilter("Binary Code (.dat)" ,".dat");
-        fc.getExtensionFilters().add(txt);
-        fc.getExtensionFilters().add(pdf);
-        fc.getExtensionFilters().add(dat);
-        fc.showOpenDialog(new Stage());
-
-        //Capturar archivo a poner en JTextArea
-        File  archivo= new File (fc.getInitialDirectory()+fc.getInitialFileName());
-        System.out.println(fc.getInitialDirectory()+fc.getInitialFileName());
-        text_area.setText("");
-        try {
-            if (archivo != null){
-                FileReader archivos= new FileReader(archivo);
-                BufferedReader leer = new BufferedReader(archivos);
-                while ((aux_lector = leer.readLine()) != null) {
-                    text_area.appendText(aux_lector+"\n");
+        text_area = new TextArea();
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Open Resource File");
+        fileChooser.getExtensionFilters().addAll(
+                new ExtensionFilter("Text Files", "*.txt"),
+                new ExtensionFilter("PDF Files","*.pdf"),
+                new ExtensionFilter("dat Files","*.dat")
+                );
+        String path_name = fileChooser.showOpenDialog(new Stage()).getPath();
+        String fill_text="";
+        if (path_name != null) {
+            try (BufferedReader br = new BufferedReader(new FileReader(path_name))) {
+                String strCurrentLine;
+                while ((strCurrentLine = br.readLine()) != null) {
+                    fill_text+=strCurrentLine+"\n";
                 }
-                leer.close();
+            } catch (IOException e) {
+                System.out.println(e.toString());;
             }
-        }catch (IOException e) {
-            System.out.println(e.toString());
+            System.out.println(fill_text);
+            text_area.appendText(fill_text);
         }
-
     }
 
     @FXML
@@ -66,5 +64,6 @@ public class Controller {
     {
         System.exit(0);
     }
+
 
 }
