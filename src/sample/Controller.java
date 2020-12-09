44bfd4d8ddf759373283ac61e5839976fa6bd8fe
itsaimejia <33639089+itsaimejia@users.noreply.github.com>
2020-12-08 22:07:55 -0800

    edicion boton salir

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,13 +1,20 @@
 package sample;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.*;
+import javafx.scene.control.ButtonBar;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.TextArea;
 import javafx.stage.FileChooser;
 import javafx.stage.FileChooser.ExtensionFilter;
 import javafx.stage.Stage;
 
 import java.io.*;
+import java.util.Optional;
 
 public class Controller {
 
@@ -19,8 +26,9 @@ public class Controller {
     boolean creado =false;
     static FileChooser fileChooser = new FileChooser();
     static String path_actual;
-    static Alert alert = new Alert(Alert.AlertType.WARNING);
+    static Alert alert = new Alert(AlertType.WARNING);
     static String initial_path = System.getProperty("user.home");
+    static String last_text="";
 
 
     public Controller() {
@@ -51,6 +59,7 @@ public class Controller {
                     fill_text += strCurrentLine + "\n";
                 }
                 area.appendText(fill_text);
+                last_text=area.getText();
                 creado = true;
             }else{
                 alert.setTitle("Open");
@@ -86,21 +95,87 @@ public class Controller {
     @FXML
     private void onNew() throws  IOException
     {
-        try{
-            File userDirectory = new File(initial_path);
+
+        Alert alert = new Alert(AlertType.CONFIRMATION);
+        alert.setTitle("Abrir nueva ventana");
+        alert.setHeaderText("Quieres abrir el archivo en una nueva ventana?");
+        alert.setContentText("Escoge tu opcion");
+
+        ButtonType new_window = new ButtonType("Nueva ventana");
+        ButtonType this_window = new ButtonType("Esta ventana");
+        ButtonType cancel = new ButtonType("Cancel", ButtonBar.ButtonData.CANCEL_CLOSE);
+
+        alert.getButtonTypes().setAll(new_window, this_window, cancel);
+
+        Optional<ButtonType> result = alert.showAndWait();
+        if (result.get() == new_window){
+            try{
+
+                Stage primaryStage = new Stage();
+                Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+                primaryStage.setTitle("Editor de texto");
+                primaryStage.setScene(new Scene(root));
+                primaryStage.show();
+
+            }catch(Exception e){
+            }
+        } else if (result.get() == this_window) {
+            /*File userDirectory = new File(initial_path);
             fileChooser.setInitialDirectory(userDirectory);
             fileChooser.setTitle("Nuevo archivo");
             area=text_area;
-            path_actual = fileChooser.showSaveDialog(area.getScene().getWindow()).getPath();
+            path_actual = fileChooser.showSaveDialog(area.getScene().getWindow()).getPath();*/
             area.setText("");
-            creado=true;
-        }catch(Exception e){
+            last_text=area.getText();
+            creado=false;
         }
     }
     @FXML
     private void onClose()
     {
-        System.exit(0);
+        Alert alert = new Alert(AlertType.CONFIRMATION);
+        ButtonType btn_save;
+        ButtonType btn_exit = new ButtonType("Salir");
+
+        if(creado){
+            if(last_text.equals(area.getText())){
+                System.exit(0);
+            }else{
+                alert.setTitle("Antes de cerrar...");
+                alert.setHeaderText("Tu archivo presenta cambios");
+                alert.setContentText("Escoge una opcion");
+                btn_save = new ButtonType("Guardar cambios");
+                alert.getButtonTypes().setAll(btn_save,btn_exit);
+                Optional<ButtonType> result = alert.showAndWait();
+
+                if (result.get() == btn_exit){
+                    System.exit(0);
+                }else if(result.get() == btn_save){
+                    try{
+                        saveFile();
+                        System.exit(0);
+                    }catch (Exception E){
+                    }
+                }
+            }
+
+        }else{
+            alert.setHeaderText("No has guardado tu archivo");
+            alert.setContentText("Escoge una opcion");
+            btn_save = new ButtonType("Guardar");
+            alert.getButtonTypes().setAll(btn_save,btn_exit);
+            Optional<ButtonType> result = alert.showAndWait();
+
+            if (result.get() == btn_exit){
+                System.exit(0);
+            }else if(result.get() == btn_save){
+                try{
+                    saveAsFile();
+
+                }catch (Exception E){
+                }
+            }
+        }
     }
 
     private void saveAsFile() throws  IOException{
@@ -115,6 +190,7 @@ public class Controller {
                 FileWriter fw = new FileWriter(fichero);
                 fw.write(area.getText());
             }
+            creado=true;
         }catch(Exception e){
         }
     }
