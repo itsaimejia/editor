73a9d6392af026e1e4304b4e5a2e6a29718de044
itsaimejia <33639089+itsaimejia@users.noreply.github.com>
2020-12-08 17:35:57 -0800

    reacomodo carpetas
    se quitaron archivos innecesarios

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,133 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.TextArea;
+import javafx.stage.FileChooser;
+import javafx.stage.FileChooser.ExtensionFilter;
+import javafx.stage.Stage;
+
+import java.io.*;
+
 public class Controller {
+
+    @FXML
+    private TextArea text_area;
+    static TextArea area;
+
+
+    boolean creado =false;
+    static FileChooser fileChooser = new FileChooser();
+    static String path_actual;
+    public Controller() {
+
+
+        fileChooser.getExtensionFilters().addAll(
+                new ExtensionFilter("Text Files", "*.txt"),
+                new ExtensionFilter("PDF Files","*.pdf"),
+                new ExtensionFilter("dat Files","*.dat"),
+                new ExtensionFilter("all files",".")
+        );
+
+    }
+
+    @FXML
+    private void onOpen()
+    {
+        fileChooser.setTitle("Seleccionar archivo");
+        String path_name = fileChooser.showOpenDialog(new Stage()).getPath();
+        path_actual =path_name;
+        String fill_text="";
+        area=text_area;
+        area.setText("");
+        if (path_name != null) {
+            try (BufferedReader br = new BufferedReader(new FileReader(path_name))) {
+                String strCurrentLine;
+                while ((strCurrentLine = br.readLine()) != null) {
+                    fill_text+=strCurrentLine+"\n";
+
+                }
+            } catch (IOException e) {
+                System.out.println(e.toString());
+            }
+
+            area.appendText(fill_text);
+            creado=true;
+        }
+    }
+
+    @FXML
+    private void onSave()
+    {
+        if(creado)
+        {
+            try
+            {
+                FileWriter fw = new FileWriter(path_actual,false);
+                fw.write(area.getText());
+                fw.close();
+            }
+            catch(IOException e)
+            {
+                e.printStackTrace();
+            }
+        }
+        else
+        {
+            System.out.println("No se ha seleccionado archivo");
+        }
+
+    }
+
+    @FXML
+    private void onSaveas()
+    {
+
+        try{
+            File userDirectory = new File("C:");
+            fileChooser.setInitialDirectory(userDirectory);
+            fileChooser.setTitle("Guardar archivo");
+            area=text_area;
+            File fichero = fileChooser.showSaveDialog(area.getScene().getWindow());
+            path_actual =fichero.getPath();
+            if(fichero != null)
+            {
+                try(FileWriter fw = new FileWriter(fichero)){
+                    fw.write(area.getText());
+                    creado =true;
+                }catch(final IOException ex){
+                    ex.printStackTrace();
+                }
+
+            }
+        }catch(Exception e){
+            System.out.println(e.toString());
+        }
+
+    }
+    @FXML
+    private void onNew()
+    {
+        try{
+            String initial_path = System.getProperty("user.home");
+            File userDirectory = new File(initial_path);
+            fileChooser.setInitialDirectory(userDirectory);
+            fileChooser.setTitle("Nuevo archivo");
+            area=text_area;
+            File fichero = fileChooser.showSaveDialog(area.getScene().getWindow());
+            path_actual = fichero.getPath();
+            area.setText("");
+            creado=true;
+        }catch(Exception e){
+            System.out.println(e.toString());
+        }
+    }
+    @FXML
+    private void onClose()
+    {
+        System.exit(0);
+    }
+
+
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,51 +1,23 @@
 package sample;
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import javafx.scene.control.MenuBar;
-import javafx.scene.control.MenuItem;
-import javafx.scene.control.Menu;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.Button;
-import javafx.scene.layout.VBox;
+
+
 
 
 public class Main extends Application {
-
-
-
 
     @Override
     public void start(Stage primaryStage) throws Exception{
 
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Editor de texto");
-
-        TextArea textArea = new TextArea();
-        Button imprimir = new Button("Imprimir");
-        imprimir.setMinWidth(50);
-
-        MenuBar menuBar = new MenuBar();
-        VBox vBox = new VBox(menuBar, textArea, imprimir);
-        Scene scene = new Scene(vBox, 500, 300);
-
-        primaryStage.setScene(scene);
+        primaryStage.setScene(new Scene(root));
         primaryStage.show();
 
-        Menu menu = new Menu("File");
-        MenuItem abrir = new MenuItem("Open");
-        MenuItem guardar = new MenuItem("Save");
-        MenuItem guardarComo = new MenuItem("Save as");
-        MenuItem nuevo = new MenuItem("New");
-
-        menu.getItems().add(abrir);
-        menu.getItems().add(guardar);
-        menu.getItems().add(guardarComo);
-        menu.getItems().add(nuevo);
-        menuBar.getMenus().add(menu);
     }
 
 
