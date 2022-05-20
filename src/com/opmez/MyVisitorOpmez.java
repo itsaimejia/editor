f5c57e1f3c6b3eba77bf1c55ee918c9f7825bad4
Itsai <itsai@ensenada.gob.mx>
2022-05-19 23:36:50 -0700

    if-else jasmin
    mayor que
    menor que
    igual que
    diferente de
    mayor igual
    menor igual

--- src/com/opmez/MyVisitorOpmez.java
+++ src/com/opmez/MyVisitorOpmez.java
@@ -4,15 +4,12 @@ import com.opmez.parser.OpmezBaseVisitor;
 import com.opmez.parser.OpmezParser;
 
 import java.io.PrintStream;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
-import java.util.List;
 
 public class MyVisitorOpmez extends OpmezBaseVisitor<Object> {
     public static HashMap<String, Object> memory = new LinkedHashMap<>();
     public static HashMap<String, Object> tempMemory = new LinkedHashMap<>();
-    public static List<String> compilador = new ArrayList<>();
     boolean joinIfElse = false;
     int fails = 0;
     private boolean bodyInScope=false;
@@ -25,16 +22,11 @@ public class MyVisitorOpmez extends OpmezBaseVisitor<Object> {
     }
     @Override
     public Object visitCuerpo(OpmezParser.CuerpoContext ctx) {
-        compilador.add(".class public Codigo");
-        compilador.add(".super java/lang/Object");
-        compilador.add(".method public static main([Ljava/lang/String;)V");
-        compilador.add(".limit stack 10");
-        compilador.add(".limit locals 10");
+
         for (int i = 0; i < ctx.instructions().size(); i++) {
             visit(ctx.instructions(i));
         }
-        compilador.add("return");
-        compilador.add(".end method");
+
         return null;
     }
 
@@ -93,12 +85,7 @@ public class MyVisitorOpmez extends OpmezBaseVisitor<Object> {
                 }
             }else if(memory.containsKey(id)) {
                 memory.put(id, value);
-                int pos=0;
-                for (String key: memory.keySet()) {
-                    if(key.equals(id)) break;
-                    pos++;
-                }
-                compilador.add("istore "+pos);
+
                 return memory.get(id);
             }
         }catch (Exception e){
@@ -110,16 +97,14 @@ public class MyVisitorOpmez extends OpmezBaseVisitor<Object> {
 
     @Override
     public Object visitImpresion(OpmezParser.ImpresionContext ctx) {
-        compilador.add("getstatic java/lang/System/out Ljava/io/PrintStream;");
         Object result = visit(ctx.expr());
-        compilador.add("invokevirtual java/io/PrintStream/println(I)V");
         System.out.println(result);
         return null;
     }
 
     @Override
     public Object visitNumero(OpmezParser.NumeroContext ctx) {
-        compilador.add("bipush "+ctx.getText());
+
         return Integer.valueOf(ctx.getText());
     }
 
@@ -135,12 +120,7 @@ public class MyVisitorOpmez extends OpmezBaseVisitor<Object> {
                 }
             }else if(memory.containsKey(id)) {
 
-                int pos=0;
-                for (String key: memory.keySet()) {
-                    if(key.equals(id)) break;
-                    pos++;
-                }
-                compilador.add("iload "+pos);
+
                 return memory.get(id);
             }else{
                 return null;
@@ -154,14 +134,12 @@ public class MyVisitorOpmez extends OpmezBaseVisitor<Object> {
     public Object visitMultDiv(OpmezParser.MultDivContext ctx) {
         int left = (int) visit(ctx.expr(0));
         int right = (int) visit(ctx.expr(1));
-        compilador.add(ctx.op.getType() == OpmezParser.MULT?"imul":"idiv");
         return (ctx.op.getType() == OpmezParser.MULT)? left * right: left / right;
     }
     @Override
     public Object visitSumSub(OpmezParser.SumSubContext ctx) {
         int left = (int) visit(ctx.expr(0));
         int right = (int) visit(ctx.expr(1));
-        compilador.add(ctx.op.getType() == OpmezParser.SUM?"iadd":"isub");
         return (ctx.op.getType() == OpmezParser.SUM) ? left + right : left - right;
     }
 
@@ -264,6 +242,7 @@ public class MyVisitorOpmez extends OpmezBaseVisitor<Object> {
     public Object visitCondicionesIgualdad(OpmezParser.CondicionesIgualdadContext ctx) {
         boolean left = (boolean) visit(ctx.condition(0));
         boolean right = (boolean) visit(ctx.condition(1));
+
         return (ctx.op.getType() == OpmezParser.EQT) ? (left == right) : (left != right);
     }
 
@@ -273,13 +252,14 @@ public class MyVisitorOpmez extends OpmezBaseVisitor<Object> {
         int right = (int) visit(ctx.expr(1));
         return (ctx.op.getType() == OpmezParser.EQT) ? (left == right) : (left != right);
     }
-
     @Override
     public Object visitCondicionesMayMen(OpmezParser.CondicionesMayMenContext ctx) {
         int left = (int) visit(ctx.expr(0));
         int right = (int) visit(ctx.expr(1));
         return (ctx.op.getType() == OpmezParser.GT) ? (left > right) : (left < right);
+
     }
+
 
     @Override
     public Object visitCondicionesMayMenIgual(OpmezParser.CondicionesMayMenIgualContext ctx) {
--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -67,7 +67,7 @@ public class Controller {
     }
 
     private void createFileJasmin(List<String> compilador) throws IOException{
-        String file_in ="C:\\Users\\itsai\\Desktop\\jasmin\\code.j";
+        String file_in ="C:\\jasmin\\code.j";
         FileWriter fw = new FileWriter(file_in,false);
         for (String line: compilador) {
             fw.write(line+"\n");
@@ -137,11 +137,10 @@ public class Controller {
             ParseTree arbol_ = sintactico_.program();
             CheckOpmez visitas_ = new CheckOpmez(ps);
             visitas_.visit(arbol_);
-            System.out.println(CheckOpmez.memory);
-            System.out.println(CheckOpmez.tempMemory);
+            createFileJasmin(CheckOpmez.compilador);
             CheckOpmez.memory.clear();
             CheckOpmez.tempMemory.clear();
-
+            CheckOpmez.compilador.clear();
             if(visitas_.errors == 0){
 
                 text_Output.clear();
@@ -152,10 +151,8 @@ public class Controller {
                 ParseTree arbol = sintactico.program();
                 MyVisitorOpmez visitas = new MyVisitorOpmez(ps);
                 visitas.visit(arbol);
-                createFileJasmin(MyVisitorOpmez.compilador);
                 MyVisitorOpmez.memory.clear();
                 MyVisitorOpmez.tempMemory.clear();
-                MyVisitorOpmez.compilador.clear();
             }else{
                 ps.println("No se pudo compilar");
             }
